ec333bd34582968096fa10241ac6cbf2fb5dd37e
rivoningo-ndubane <rivinine@gmail.com>
2026-02-05 15:43:46 +0200

    Complete main logic and add keyboard support

--- program.js
+++ program.js
@@ -1,3 +1,7 @@
+function round(value) {
+    return +value.toFixed(2);
+}
+
 function add(a, b){
     return a + b;
 }
@@ -20,23 +24,38 @@ function divide(a, b){
 
 function operate(operator, numOne, numTwo){
     switch(operator){
-        case "+": return add(numOne, numTwo);
-        case "-": return subtract(numOne, numTwo);
-        case "*": return multiply(numOne, numTwo);
-        case "/": return divide(numOne, numTwo);
+        case "add": return add(numOne, numTwo);
+        case "subtract": return subtract(numOne, numTwo);
+        case "multiply": return multiply(numOne, numTwo);
+        case "divide": return round(divide(numOne, numTwo));
     }
 }
+
 
 // Variables
 let numOne = 0;
 let numTwo = 0;
-let operator = '';
+let operator = "";
+let canClear = false;
+let secondTyped = false;
 
+// Reference to inputBox and history
+const inputBox = document.getElementById("inputBox");
+const history = document.getElementById("history");
+
+// Focus on input box
+inputBox.focus();
+
+// Event handlers
 const bottom = document.querySelector(".bottom");
 bottom.addEventListener('click', (e) => {
     // Handle number input
     if (e.target.className.includes("number")) {
-        const inputBox = document.getElementById("inputBox");
+        if (canClear) {
+            inputBox.value = "";
+            canClear = false;
+            secondTyped = true;
+        }
 
         if (inputBox.value === "0"){
             if (e.target.textContent !== "0"){
@@ -49,23 +68,240 @@ bottom.addEventListener('click', (e) => {
         
     }
 
+    // Handle decimal input
+    outer:if (e.target.id === "decimal") {
+        if (inputBox.value.includes(".")) {
+            break outer;
+        }
+        inputBox.value += ".";
+    }
+
     // Handle operators
-    switch(e.target.id){
-        case "add": {
-            break;
+    outer:if (e.target.className.includes("operator")) {
+        if (secondTyped){
+            secondTyped = false;
+            e.target.id = "equal";
+            break outer;
         }
 
-        case "subtract": {
-            break;
+        if (operator !== "") {
+            operator = e.target.id;
+        }
+        else {
+            numOne = parseFloat(inputBox.value);
+            operator = e.target.id;
         }
 
-        case "multiply": {
-            break;
+        history.textContent = numOne;
+        switch(operator){
+            case "add": {
+                history.textContent += ` + `;
+                break;
+            }
+
+            case "subtract": {
+                history.textContent += ` - `;
+                break;
+            }
+
+            case "multiply": {
+                history.textContent += ` x `;
+                break;
+            }
+
+            case "divide": {
+                history.textContent += ` \u00F7 `;
+                break;
+            }
         }
 
-        case "divide": {
-            break;
+        // Update control variable
+        canClear = true;
+    }
+
+    // Handle equal operator
+    outer:if (e.target.id === "equal") {
+        if (operator === "") {
+            history.textContent = `${inputBox.value} =`;
+            break outer;
         }
+        numTwo = parseFloat(inputBox.value);
+        let result = operate(operator, numOne, numTwo);
+
+        // Update displays
+        history.textContent += `${numTwo} =`;
+        inputBox.value = result;
+
+        // Reset operator and control flag
+        operator = "";
+        secondTyped = false;
+        
     }
     
+    // Handle backspace
+    outer:if (e.target.id === "backspace") {
+        if (inputBox.value === "0" ){
+            break outer;
+        }
+
+        // Backspace logic
+        inputBox.value = inputBox.value.slice(0, -1);
+
+        if (inputBox.value === "") {
+            inputBox.value = 0;
+        }
+    }
+
+    // Handle clear
+    if (e.target.id === "clear") {
+        numOne = 0;
+        numTwo = 0;
+        operator = "";
+        history.textContent = "";
+        inputBox.value = 0;
+    }
+
+    // Refocus on inputBox
+    inputBox.focus();
+})
+
+//----------------------------------------------------------------------
+// Keyboard support
+inputBox.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    console.log(e.key);
+    // Check for numbers pressed
+    if (Number.isInteger(+(e.key))) {
+        if (canClear) {
+            inputBox.value = "";
+            canClear = false;
+            secondTyped = true;
+        }
+
+        if (inputBox.value === "0"){
+            if (e.key !== "0"){
+                inputBox.value = e.key;
+            }
+        }
+        else{
+            inputBox.value += e.key;
+        }
+    }
+
+    // Handle decimal input
+    outer:if (e.key === ".") {
+        if (inputBox.value.includes(".")) {
+            break outer;
+        }
+        inputBox.value += ".";
+    }
+
+    // Handle backspace
+    outer:if (e.key === "Backspace") {
+        if (inputBox.value === "0" ){
+            break outer;
+        }
+
+        // Backspace logic
+        inputBox.value = inputBox.value.slice(0, -1);
+
+        if (inputBox.value === "") {
+            inputBox.value = 0;
+        }
+    }
+
+    // Handle operators
+    outer:if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+        if (secondTyped){
+            secondTyped = false;
+            e.key = "Enter";
+            break outer;
+        }
+
+        if (operator !== ""){
+            
+        }
+        else {
+            numOne = parseFloat(inputBox.value);
+        }
+
+        switch(e.key){
+            case "+":{
+                operator = "add";
+                break;
+            }
+
+            case "-":{
+                operator = "subtract";
+                break;
+            } 
+
+            case "*":{
+                operator = "multiply";
+                break;
+            }
+
+            case "/": {
+                operator = "divide";
+                break;
+            }
+        }
+        
+        history.textContent = numOne;
+        switch(operator){
+            case "add": {
+                history.textContent += ` + `;
+                break;
+            }
+
+            case "subtract": {
+                history.textContent += ` - `;
+                break;
+            }
+
+            case "multiply": {
+                history.textContent += ` x `;
+                break;
+            }
+
+            case "divide": {
+                history.textContent += ` \u00F7 `;
+                break;
+            }
+        }
+
+        // Update control variable
+        canClear = true;
+    }
+
+    // Handle equal operator
+    outer:if (e.key === "Enter" || e.key === "=") {
+        if (operator === "") {
+            history.textContent = `${inputBox.value} =`;
+            break outer;
+        }
+        numTwo = parseFloat(inputBox.value);
+        let result = operate(operator, numOne, numTwo);
+
+        // Update displays
+        history.textContent += `${numTwo} =`;
+        inputBox.value = result;
+
+        // Reset operator and control flag
+        operator = "";
+        secondTyped = false;
+        
+    }
+
+     // Handle clear
+    if (e.key === "Escape") {
+        numOne = 0;
+        numTwo = 0;
+        operator = "";
+        history.textContent = "";
+        inputBox.value = 0;
+    }
+
+    // Refocus on inputBox
+    inputBox.focus();
 })
